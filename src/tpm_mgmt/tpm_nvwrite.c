862fee62a78bfa700d90dad36a8cdae826ddb0d2
Kent Yoder <key@linux.vnet.ibm.com>
2013-02-06 10:52:12 -0600

    tpm_mgmt/tpm_nvwrite.c: fixed 2 missing free() calls and a test if an unsigned was less than 0.
    Signed-off-by: Kent Yoder <key@linux.vnet.ibm.com>

--- src/tpm_mgmt/tpm_nvwrite.c
+++ src/tpm_mgmt/tpm_nvwrite.c
@@ -209,21 +209,19 @@ int main(int argc, char **argv)
 		fd = open(filename, O_RDONLY);
 		if (fd < 0) {
 			logError(_("Could not open file %s for reading.\n"));
+			free(rgbDataToWrite);
 			return -1;
 		}
 		read_bytes = read(fd, rgbDataToWrite, ulDataLength);
 
 		if (read_bytes < 0 || ulDataLength != (UINT32)read_bytes) {
 			logError(_("Error while reading data.\n"));
+			free(rgbDataToWrite);
 			return -1;
 		}
 		close(fd);
 		fd = -1;
 	} else if (fillvalue >= 0) {
-		if (length < 0) {
-			logError(_("Requiring size parameter.\n"));
-			return -1;
-		}
 		ulDataLength = length;
 		rgbDataToWrite = malloc(ulDataLength);
 		if (rgbDataToWrite == NULL) {
